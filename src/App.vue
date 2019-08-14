1d3f2de59aa0a6b88c6a78cd276477f7f9fe7041
Finn Kairies <finn.kairies@gmail.com>
2019-08-14 20:59:37 +0200

    Addad Side-Heading

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1 class="side-heading">Aethixx</h1>
     <Header />
     <div class="main-container">
       <Heading />
@@ -24,6 +25,23 @@ export default {
 </script>
 
 <style>
+.side-heading {
+  position: absolute;
+  width: 530px;
+  height: 151px;
+  top: 450px;
+  right: 70px;
+  font-family: "Apercu";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 90px;
+  line-height: 111px;
+  letter-spacing: 0.33em;
+  background: none;
+  color: rgba(255, 255, 255, 0.22);
+
+  transform: rotate(-90deg);
+}
 .main-container {
   padding: 67px;
 }
@@ -35,6 +53,7 @@ export default {
 #app {
   background: #000;
   height: 100vh;
+  position: relative;
 }
 </style>>
 
